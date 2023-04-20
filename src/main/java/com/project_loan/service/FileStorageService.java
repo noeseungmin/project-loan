d0ba5a2c844715c46920694a845f50c6d5aa6a24
seungmin <nowinmin96@gmail.com>
2023-04-20 18:00:31 +0900

    #5-대출 신청 서류
    대출 신청 서류 등록, 조회, 수정 , 삭제 기능 개발

---
+++ src/main/java/com/project_loan/service/FileStorageService.java
@@ -0,0 +1,19 @@
+package com.project_loan.service;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public interface FileStorageService {
+
+    void save(Long applicationId, MultipartFile file);
+
+    Resource load(Long applicationId, String fileName);
+
+    Stream<Path> loadAll(Long applicationId);
+
+    void deleteAll(Long applicationId);
+
+}
