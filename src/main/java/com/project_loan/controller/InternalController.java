1ffcb2153541fe97d7a1807fedf5bff9bf2fe535
seungmin <nowinmin96@gmail.com>
2023-05-10 19:54:53 +0900

    #11-대출 상환 기능
    대출금 상환 금액 처리, api 등록

--- src/main/java/com/project_loan/controller/InternalController.java
+++ src/main/java/com/project_loan/controller/InternalController.java
@@ -1,10 +1,17 @@
 package com.project_loan.controller;
 
 import com.project_loan.dto.EntryDto;
+import com.project_loan.dto.RepaymentDto;
+import com.project_loan.dto.RepaymentDto.ListResponse;
+import com.project_loan.dto.RepaymentDto.Request;
+import com.project_loan.dto.RepaymentDto.Response;
 import com.project_loan.dto.ResponseDto;
 import com.project_loan.service.EntryService;
+import com.project_loan.service.RepaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -12,6 +19,8 @@ import org.springframework.web.bind.annotation.*;
 public class InternalController extends AbstractController{
 
     private final EntryService entryService;
+
+    private final RepaymentService repaymentService;
 
    @PostMapping("/{applicationId}/entries")
     public ResponseDto<EntryDto.Response> create(@PathVariable Long applicationId, @RequestBody EntryDto.Request request){
@@ -31,6 +40,28 @@ public class InternalController extends AbstractController{
     @DeleteMapping("/entries/{entryId}")
     public ResponseDto<Void> delete(@PathVariable Long entryId){
        entryService.delete(entryId);
+       return ok();
+    }
+
+    @PostMapping("/{applicationId}/repayments")
+    public ResponseDto<Response> create(@PathVariable Long applicationId, @RequestBody Request request){
+    return ok(repaymentService.create(applicationId, request));
+    }
+
+    @GetMapping("/{applicationId}/repayments")
+    public ResponseDto<List<ListResponse>> getRepayments(@PathVariable Long applicationId){
+       return ok(repaymentService.get(applicationId));
+    }
+
+    @PutMapping("/repayments/{repaymentId}")
+    public ResponseDto<RepaymentDto.UpdateResponse> update(@PathVariable Long repaymentId,
+                                                           @RequestBody Request request){
+       return ok(repaymentService.update(repaymentId, request));
+    }
+
+    @DeleteMapping("/repayments/{repaymentId}")
+    public ResponseDto<Void> deleteRepayment(@PathVariable Long repaymentId){
+       repaymentService.delete(repaymentId);
        return ok();
     }
 }
--- src/main/java/com/project_loan/service/BalanceService.java
+++ src/main/java/com/project_loan/service/BalanceService.java
@@ -1,6 +1,7 @@
 package com.project_loan.service;
 
 import com.project_loan.dto.BalanceDto;
+import com.project_loan.dto.BalanceDto.RepaymentRequest;
 import com.project_loan.dto.BalanceDto.Request;
 import com.project_loan.dto.BalanceDto.Response;
 import com.project_loan.dto.BalanceDto.UpdateRequest;
@@ -10,4 +11,6 @@ public interface BalanceService {
     Response create(Long applicationId, Request request);
 
     Response update(Long applicationId, UpdateRequest request);
+
+    Response repaymentUpdate(Long applicationId, RepaymentRequest request);
 }
